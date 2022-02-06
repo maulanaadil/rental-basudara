3ea67f01c037f64fcd80ae0a4636f1240120219f
Maulana Adil <maulanaadil.latif@gmail.com>
2022-02-06 15:48:10 +0700

    add tampilan client buat yang ingin menyewa

--- index.php
+++ index.php
@@ -18,8 +18,9 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
-        <div class="row mb-2 justify-content-center">
+        <div class="row mb-2 justify-content-center d-block">
             <img sr="./public/asseets/logo.png" alt="Ini ceritanya gambar" class="img-fluid w-100">
+            <h4 class="h5 text-center font-weight-light">Silahkan memilih jenis Playstation dan tanggal penyewaannya <br />lalu isi identitas anda. </h3>
         </div>
       </div><!-- /.container-fluid -->
     </section>
@@ -27,10 +28,56 @@
     <!-- Main content -->
     <section class="container">
         <div class="row justify-content-center">
-            
-        </div>
-      
+            <div class="card">
+                <div class="card-body">
+                    <form action="#" method="post">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text" for="nama">Nama</label>
+                        </div>
+                        <input type="text" name="nama" id="nama" class="form-control" placeholder="Masukan nama" aria-label="nama">
+                    </div>
+                    
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text" for="no-hp">No Handphone</label>
+                        </div>
+                        <input type="tel" name="no-hp" id="no-hp" class="form-control" placeholder="Masukan no hanphone" aria-label="no-hp">
+                    </div>
 
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text" for="email">Email</label>
+                        </div>
+                        <input type="email" name="email" id="email" class="form-control" placeholder="Masukan email" aria-label="email">
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text" for="jenis-playstation">Jenis Playstation</label>
+                        </div>
+                    <select class="custom-select" id="jenis-playstation" name="jenis-playstation">
+                        <option selected>Pilih Playstation</option>
+                        <option value="Playstation 3">Playstation 3</option>
+                        <option value="Playstation 4">Playstation 4</option>
+                        <option value="Playstation 5">Playstation 5</option>
+                    </select>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text" for="tanggal">Tanggal Sewa</label>
+                        </div>
+                        <input type="date" name="tanggal" id="tanggal" class="form-control" placeholder="Pilih Tanggal" aria-label="tanggal">
+                    </div>
+                    <div class="d-inline float-right">
+                        <input type="reset" class="btn btn-outline btn-danger">
+                        <input type="submit" class="btn btn-primary">
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </section>
     <!-- /.content -->
 
