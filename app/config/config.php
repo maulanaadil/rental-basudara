f4be77c546f00564eeb909b8793a11113d1c16e8
Maulana Adil <maulanaadil.latif@gmail.com>
2022-02-06 13:36:17 +0700

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('base_url', 'http://localhost/rental-basudara/public');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'rental_basudara');
