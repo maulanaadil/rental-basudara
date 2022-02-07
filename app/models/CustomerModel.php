75ff57487926e3fdc522ae8c989d9423541eb9c3
denden-tri <83654878+denden-tri@users.noreply.github.com>
2022-02-07 18:46:39 +0700

    edit tambahCustomer agar bisa sekaligus, menambah data di table transaksi

--- app/models/CustomerModel.php
+++ app/models/CustomerModel.php
@@ -1,13 +1,17 @@
 <?php
 
-class CustomerModel {
-	
+class CustomerModel
+{
+
 	private $table = 'customer';
 	private $db;
+	private $transaksi;
 
 	public function __construct()
 	{
+		require_once(__DIR__ . '/TransaksiModel.php');
 		$this->db = new Database;
+		$this->transaksi = new TransaksiModel;
 	}
 
 	public function getCustomerById($customer_id)
@@ -19,13 +23,34 @@ class CustomerModel {
 
 	public function tambahCustomer($data)
 	{
-		$query = "INSERT INTO customer (customer_id, nama, no_hp, email) VALUES(:customer_id, :nama, :no_hp, :email)";
+		// Tambah Customer
+		$query = "INSERT INTO customer ( nama, no_hp, email) VALUES( :nama, :no_hp, :email)";
 		$this->db->query($query);
 		$this->db->bind('nama', $data['nama']);
 		$this->db->bind('no_hp', $data['no_hp']);
 		$this->db->bind('email', $data['email']);
 		$this->db->execute();
 
+		// Query 'customer_id' terbaru
+		$this->db->query("SELECT MAX(customer_id) FROM " . $this->table);
+		$this->db->execute();
+
+		// Memasukan 'customer_id' ke array $data
+		$customer_id = $this->getCustomerIdMax();
+		$data['customer_id'] = $customer_id;
+
+		// Menambahkan data baru ke tabel 'transaksi'
+		$this->transaksi->tambahTransaksi($data);
+
 		return $this->db->rowCount();
+	}
+
+	public function getCustomerIdMax()
+	{
+		$this->db->query("SELECT MAX(customer_id) as max_customer_id FROM " . $this->table);
+		$this->db->execute();
+		$data = $this->db->single();
+
+		return $data['max_customer_id'];
 	}
 }
