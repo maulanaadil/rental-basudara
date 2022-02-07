7b99aa6a0d5913bd8eaa5417568aea562bbeb45f
denden-tri <83654878+denden-tri@users.noreply.github.com>
2022-02-07 18:47:08 +0700

    add tambahTransaksi method

--- app/models/TransaksiModel.php
+++ app/models/TransaksiModel.php
@@ -1,12 +1,16 @@
 <?php
 
-class TransaksiModel {
-	
+class TransaksiModel
+{
+
 	private $table = 'transaksi';
 	private $db;
+	private $playstation;
 
 	public function __construct()
 	{
+		require_once(__DIR__ . '/PlaystationModel.php');
+		$this->playstation = new PlaystationModel;
 		$this->db = new Database;
 	}
 
@@ -19,7 +23,42 @@ class TransaksiModel {
 	public function getTransaksiById($transaksi_id)
 	{
 		$this->db->query('SELECT * FROM ' . $this->table . 'WHERE transaksi_id:=transaksi_id');
-        $this->db->bind('transaksi_id', $transaksi_id);
+		$this->db->bind('transaksi_id', $transaksi_id);
 		return $this->db->single();
-	}	
+	}
+
+	public function tambahTransaksi($data)
+	{
+		// Menentukan perbedaan berapa hari dari 'tanggal' dan 'tanggal_kembali'
+		$date1 = new DateTime($data['tanggal']);
+		$date2 = new DateTime($data['tanggal_kembali']);
+		$interval = $date1->diff($date2);
+		$bedaTanggal = $interval->days;
+
+		// TODO: Menentukan harga dari playstation yang tersedia dan sesuai jenis yang dipilih customer
+		// TODO: Merubah 'status_peminjaman' menjadi 'dipinjam'
+		$ps_id = $this->playstation->getPlaystationTersediaByJenis($data['jenis-playstation']);
+		if (is_null($ps_id['ps_id'])) {
+			// Flasher::setMessage($ps_id['msg'], "dicari", "danger");
+			return 0;
+		} else {
+			$res_ps = $this->playstation->getPlaystationById($ps_id['ps_id']);
+			$harga = $res_ps['harga'];
+			$this->playstation->setPlaystationStatusPeminjaman($ps_id, "dipinjam");
+		}
+
+		$query = "INSERT INTO `rental_basudara`.`transaksi` (`customer_id`, `ps_id`, `pegawai_id`, `tanggal_pinjam`, `tanggal_kembali`, `total`, `status_transaksi`, `denda`) VALUES (:customer_id, :ps_id, :pegawai_id, :tgl_pinjam, :tgl_kembali, :total, :status_transaksi, :denda)";
+		$this->db->query($query);
+		$this->db->bind('customer_id', $data['customer_id']);
+		$this->db->bind('ps_id', 'P1');
+		$this->db->bind('pegawai_id', 'A1');
+		$this->db->bind('tgl_pinjam', $data['tanggal']);
+		$this->db->bind('tgl_kembali', $data['tanggal_kembali']);
+		$this->db->bind('total', $bedaTanggal * $harga);
+		$this->db->bind('status_transaksi', NULL);
+		$this->db->bind('denda', 0);
+		$this->db->execute();
+
+		return $this->db->single();
+	}
 }
